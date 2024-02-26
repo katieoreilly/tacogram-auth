a37c9abddfd7e182ab866d62588d78d914d1567b
Katie O'Reilly <katie.oreilly@kellogg.northwestern.edu>
2024-02-26 22:15:21 +0000

    bug fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,24 @@
 class PostsController < ApplicationController
 
-    def index
-      @posts = Post.all
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
-      @user = User.find_by({"id" => session["user_id]"]})
-      if @user != nil
-        @post = Post.new
-        @post["body"] = params["body"]
-        @post["image"] = params["image"]
-        # TODO: assign logged-in user as user that created the post
-        @post["user_id"] = @user["user_id"]
-        @post.save
-      else
-        flash["notice"] = "Login first."
-      end
-      redirect_to "/posts"
-    end
-  
+  def index
+    @posts = Post.all
   end
+
+  def new
+  end
+
+  def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @post = Post.new
+      @post["body"] = params["body"]
+      @post["image"] = params["image"]
+      @post["user_id"] = @user["id"]
+      @post.save
+    else
+      flash["notice"] = "Login first."
+    end
+    redirect_to "/posts"
+  end
+
+end
