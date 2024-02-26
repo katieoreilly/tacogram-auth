84043e8c097c610ee524c55923f41eb669353367
Katie O'Reilly <katie.oreilly@kellogg.northwestern.edu>
2024-02-26 22:29:55 +0000

    more fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,20 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
-    if User.find_by({"email" => params["email"] }) == nil
+    if User.find_by({ "email" => params["email"] }) == nil
       @user = User.new
       @user["first_name"] = params["first_name"]
       @user["last_name"] = params["last_name"]
       @user["email"] = params["email"]
-      # TODO: encrypt user's password "at rest"
       @user["password"] = BCrypt::Password.create(params["password"])
       @user.save
       redirect_to "/login"
     else
       flash["notice"] = "Email taken."
-      redirect_to "/users/new" 
-    end 
+      redirect_to "/users/new"
+    end
   end
 end
+
